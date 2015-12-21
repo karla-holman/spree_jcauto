aa623964a9d6836d8c05e3e7b6174a12b75b2a80
Karla Holman <holmankarla@gmail.com>
2015-12-21 10:42:25 -0800

    Added images to sell parts, added checks to mailer for image size and quantity

--- core/app/mailers/spree/contact_mailer.rb
+++ core/app/mailers/spree/contact_mailer.rb
@@ -1,14 +1,28 @@
 module Spree
   class ContactMailer < BaseMailer
-    def contact_email(user, message)
+    def contact_email(user, message, images)
       @user = user
       @message = message
+
+      # User uploaded images
+      count = 1
+      images.each do |image|
+        attachments.inline["User-#{count}.png"] = image
+        count += 1
+      end
+
+      # JC Auto Logo
       attachments.inline['Logo.png'] = File.read(Rails.root.join("public", "Logo-new.png"))
+      
+      # determine subject based on form
       if(user[:part_numbers])
       	subject = "#{Spree::Store.current.name} Sell Request From Customer"
+      elsif(user[:quote_car])
+        subject = "#{Spree::Store.current.name} Service Request From Customer"
       else
       	subject = "#{Spree::Store.current.name} Contact From Customer"
       end
+      
       mail(to: from_address, from: user[:address], subject: subject)
     end
   end
