7cf967cc6bba917a3ccd3a4ab2511e5280657635
Karla Holman <holmankarla@gmail.com>
2016-01-19 16:20:43 -0800

    Fixed search issues with punctuation and spaces

--- core/lib/spree/core/search/base.rb
+++ core/lib/spree/core/search/base.rb
@@ -180,7 +180,7 @@ module Spree
                 params[:search].delete(:name_or_meta_keywords_or_description_or_product_properties_value_cont_any)
                 params[:search][:name_or_meta_keywords_or_description_or_product_properties_value_cont] = keyword_string
               else
-              params[:search][:name_or_meta_keywords_or_description_or_product_properties_value_cont_any] = params[:search][:name_or_meta_keywords_or_description_or_product_properties_value_cont_any].split
+              params[:search][:name_or_meta_keywords_or_description_or_product_properties_value_cont_any] = params[:search][:name_or_meta_keywords_or_description_or_product_properties_value_cont_any].gsub(/\W/, '').split
               end
             end
             @properties[:search] = params[:search] ? params[:search].reject{|_, v| v == ""} : params[:search] # don't take empty search filters
