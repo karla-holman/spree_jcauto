46b4b76135231e26755fea70ba86d4b547b6b6ec
Karla Holman <holmankarla@gmail.com>
2016-03-03 12:34:10 -0800

    Added flat percent with min calculator

---
+++ core/app/models/spree/calculator/shipping/flat_rate_min.rb
@@ -0,0 +1,33 @@
+require_dependency 'spree/shipping_calculator'
+
+module Spree
+  module Calculator::Shipping
+    class FlatPercentItemTotalMin < ShippingCalculator
+      preference :flat_percent, :decimal, default: 0
+      preference :minimal_amount, :decimal, default: 0
+
+      def self.description
+        "Flat percent with minimal amount"
+      end
+
+      def compute_package(package)
+        compute_from_price(total(package.contents))
+      end
+
+      def compute_from_price(price)
+        # get amount to add
+        value = price * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
+
+        # get dollar value
+        dollar_val = (value * 100).round.to_f / 100
+
+        # check if minimum met
+        if dollar_val < self.preferred_minimal_amount
+          self.preferred_normal_amount
+        else
+          dollar_val
+        end
+      end
+    end
+  end
+end
