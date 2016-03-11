e07d8e3b5cc5538f7cd24aa7fe7f6bd10a0a5a47
Karla Holman <holmankarla@gmail.com>
2016-03-11 15:56:29 -0800

    fixed checkbox validation

--- core/app/models/spree/address.rb
+++ core/app/models/spree/address.rb
@@ -10,7 +10,7 @@ module Spree
     validates :firstname, :lastname, :address1, :city, :country, presence: true
     validates :zipcode, presence: true, if: :require_zipcode?
     validates :phone, presence: true, if: :require_phone?
-    validates :commercial, presence: true
+    validates_inclusion_of :commercial, :in => [true, false]
 
     validate :state_validate, :postal_code_validate
 
