6a2d54f6c2655a2b780758621026c4f3ed2375bf
Karla Holman <holmankarla@gmail.com>
2016-03-16 10:00:13 -0700

    Calculate flat rate based on each item

--- core/app/models/spree/calculator/shipping/flat_rate_insurance.rb
+++ core/app/models/spree/calculator/shipping/flat_rate_insurance.rb
@@ -72,74 +72,78 @@ module Spree
       end
 
       def compute_package(package)
-        compute_from_price(total(package.contents))
+        # compute_from_price(total(package.contents))
+        compute_from_price(package.contents)
       end
 
-      def compute_from_price(price)
+      def compute_from_price(contents)
         total_price = 0
-        if price < self.preferred_insurance_1_top
-          total_price = self.preferred_insurance_1_amount
-        elsif price < self.preferred_insurance_2_top
-          total_price = self.preferred_insurance_2_amount
-        elsif price < self.preferred_insurance_3_top
-          total_price = self.preferred_insurance_3_amount
-        elsif price < self.preferred_insurance_4_top
-          total_price = self.preferred_insurance_4_amount
-        elsif price < self.preferred_insurance_5_top
-          total_price = self.preferred_insurance_5_amount
-        elsif price < self.preferred_insurance_6_top
-          total_price = self.preferred_insurance_6_amount
-        elsif price < self.preferred_insurance_7_top
-          total_price = self.preferred_insurance_7_amount
-        elsif price < self.preferred_insurance_8_top
-          total_price = self.preferred_insurance_8_amount
-        elsif price < self.preferred_insurance_9_top
-          total_price = self.preferred_insurance_9_amount
-        elsif price < self.preferred_insurance_10_top
-          total_price = self.preferred_insurance_10_amount
-        elsif price < self.preferred_insurance_11_top
-          total_price = self.preferred_insurance_11_amount
-        elsif price < self.preferred_insurance_12_top
-          total_price = self.preferred_insurance_12_amount
-        elsif price < self.preferred_insurance_13_top
-          total_price = self.preferred_insurance_13_amount
-        elsif price < self.preferred_insurance_14_top
-          total_price = self.preferred_insurance_14_amount
-        elsif price < self.preferred_insurance_15_top
-          total_price = self.preferred_insurance_15_amount
-        elsif price < self.preferred_insurance_16_top
-          total_price = self.preferred_insurance_16_amount
-        elsif price < self.preferred_insurance_17_top
-          total_price = self.preferred_insurance_17_amount
-        elsif price < self.preferred_insurance_18_top
-          total_price = self.preferred_insurance_18_amount
-        elsif price < self.preferred_insurance_19_top
-          total_price = self.preferred_insurance_19_amount
-        elsif price < self.preferred_insurance_20_top
-          total_price = self.preferred_insurance_20_amount
-        elsif price < self.preferred_insurance_21_top
-          total_price = self.preferred_insurance_21_amount
-        elsif price < self.preferred_insurance_22_top
-          total_price = self.preferred_insurance_22_amount
-        elsif price < self.preferred_insurance_23_top
-          total_price = self.preferred_insurance_23_amount
-        elsif price < self.preferred_insurance_24_top
-          total_price = self.preferred_insurance_24_amount
-        elsif price < self.preferred_insurance_25_top
-          total_price = self.preferred_insurance_25_amount
-        elsif price < self.preferred_insurance_26_top
-          total_price = self.preferred_insurance_26_amount
-        elsif price < self.preferred_insurance_27_top
-          total_price = self.preferred_insurance_27_amount
-        elsif price < self.preferred_insurance_28_top
-          total_price = self.preferred_insurance_28_amount
-        elsif price < self.preferred_insurance_29_top
-          total_price = self.preferred_insurance_29_amount
-        elsif price < self.preferred_insurance_30_top
-          total_price = self.preferred_insurance_30_amount
-        else # Over top limit, use calculator
-          additional = self.preferred_insurance_max_base_price + ((price - self.preferred_insurance_30_top) / 100) * self.preferred_insurance_max_per
-          total_price = additional
+        contents.each do |item|
+          price = item.price
+          if price < self.preferred_insurance_1_top
+            total_price += self.preferred_insurance_1_amount
+          elsif price < self.preferred_insurance_2_top
+            total_price += self.preferred_insurance_2_amount
+          elsif price < self.preferred_insurance_3_top
+            total_price += self.preferred_insurance_3_amount
+          elsif price < self.preferred_insurance_4_top
+            total_price += self.preferred_insurance_4_amount
+          elsif price < self.preferred_insurance_5_top
+            total_price += self.preferred_insurance_5_amount
+          elsif price < self.preferred_insurance_6_top
+            total_price += self.preferred_insurance_6_amount
+          elsif price < self.preferred_insurance_7_top
+            total_price += self.preferred_insurance_7_amount
+          elsif price < self.preferred_insurance_8_top
+            total_price += self.preferred_insurance_8_amount
+          elsif price < self.preferred_insurance_9_top
+            total_price += self.preferred_insurance_9_amount
+          elsif price < self.preferred_insurance_10_top
+            total_price += self.preferred_insurance_10_amount
+          elsif price < self.preferred_insurance_11_top
+            total_price += self.preferred_insurance_11_amount
+          elsif price < self.preferred_insurance_12_top
+            total_price += self.preferred_insurance_12_amount
+          elsif price < self.preferred_insurance_13_top
+            total_price += self.preferred_insurance_13_amount
+          elsif price < self.preferred_insurance_14_top
+            total_price += self.preferred_insurance_14_amount
+          elsif price < self.preferred_insurance_15_top
+            total_price += self.preferred_insurance_15_amount
+          elsif price < self.preferred_insurance_16_top
+            total_price += self.preferred_insurance_16_amount
+          elsif price < self.preferred_insurance_17_top
+            total_price += self.preferred_insurance_17_amount
+          elsif price < self.preferred_insurance_18_top
+            total_price += self.preferred_insurance_18_amount
+          elsif price < self.preferred_insurance_19_top
+            total_price += self.preferred_insurance_19_amount
+          elsif price < self.preferred_insurance_20_top
+            total_price += self.preferred_insurance_20_amount
+          elsif price < self.preferred_insurance_21_top
+            total_price += self.preferred_insurance_21_amount
+          elsif price < self.preferred_insurance_22_top
+            total_price += self.preferred_insurance_22_amount
+          elsif price < self.preferred_insurance_23_top
+            total_price += self.preferred_insurance_23_amount
+          elsif price < self.preferred_insurance_24_top
+            total_price += self.preferred_insurance_24_amount
+          elsif price < self.preferred_insurance_25_top
+            total_price += self.preferred_insurance_25_amount
+          elsif price < self.preferred_insurance_26_top
+            total_price += self.preferred_insurance_26_amount
+          elsif price < self.preferred_insurance_27_top
+            total_price += self.preferred_insurance_27_amount
+          elsif price < self.preferred_insurance_28_top
+            total_price += self.preferred_insurance_28_amount
+          elsif price < self.preferred_insurance_29_top
+            total_price += self.preferred_insurance_29_amount
+          elsif price < self.preferred_insurance_30_top
+            total_price += self.preferred_insurance_30_amount
+          else # Over top limit, use calculator
+            additional = self.preferred_insurance_max_base_price + ((price - self.preferred_insurance_30_top) / 100) * self.preferred_insurance_max_per
+            total_price += additional
+          end
         end
         total_price
       end
